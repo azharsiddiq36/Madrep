e38d8c4b8600f0651da3fe60d8d92dc30fb5120d
azharsiddiq36 <azharsiddiq36@gmail.com>
2019-05-12 14:43:08 +0700

    Interface

---
+++ app/src/main/java/com/azhar/madrep/Activity/MainActivity.java
@@ -0,0 +1,73 @@
+package com.azhar.madrep.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.azhar.madrep.Fragment.DokterFragment;
+import com.azhar.madrep.Fragment.HomeFragment;
+import com.azhar.madrep.Fragment.KunjunganFragment;
+import com.azhar.madrep.Fragment.ObatFragment;
+import com.azhar.madrep.Fragment.ProfileFragment;
+import com.azhar.madrep.R;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.frameFragment)
+    FrameLayout frameFragment;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.nav_profile:
+                    fragment = new ProfileFragment();
+                    break;
+                case R.id.nav_dokter:
+                    fragment = new DokterFragment();
+                    break;
+                case R.id.nav_kunjungan:
+                    fragment = new KunjunganFragment();
+                    break;
+                case R.id.nav_obat:
+                    fragment = new ObatFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameFragment, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/azhar/madrep/Fragment/ObatFragment.java
@@ -0,0 +1,33 @@
+package com.azhar.madrep.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.azhar.madrep.R;
+
+import butterknife.ButterKnife;
+
+
+public class ObatFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_obat, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+}
