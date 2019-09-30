b506794458dcc2dd915e03ceb3c045678d459961
Azhar <azharsiddiq36@gmail.com>
2019-09-30 12:14:50 +0700

    Update CombineApi.java

--- app/src/main/java/com/azhar/madrep/Rest/CombineApi.java
+++ app/src/main/java/com/azhar/madrep/Rest/CombineApi.java
@@ -1,8 +1,8 @@
 package com.azhar.madrep.Rest;
 
 public class CombineApi {
-    public static final String BASE_URL = "http://produk.digtive.id/madrep/api/";
-    public static final String img_url = "http://produk.digtive.id/madrep/";
+    public static final String BASE_URL = "YOUR_API";
+    public static final String img_url = "YOUR_IMAGE_PATH";
 //    public static final String BASE_URL = "http://192.168.43.201/ci/madrep/api/";
 //    public static final String img_url = "http://192.168.43.201/ci/madrep/";
     public static MadrepInterface getApiService(){
